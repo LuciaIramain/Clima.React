71cb08ff863a8783461bb9755eb74de0439c1959
Lucia Iramain <luciairamain5@gmail.com>
2022-03-21 13:19:40 -0300

    se realizo la logica para hacer la consulta a la api del clima

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
+
+  // State del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+  // ultilizo este state para controlar la busqueda y no estar llamando a la api cada vez que presiono una tecla
+  const [consultar, guardarConsultar] = useState(false);
+  const {ciudad, pais} = busqueda;
+
+  useEffect(()=>{
+    const consultarAPI = async () => {
+      if(consultar){
+        const appId = '75f59b7f3b766efe0b416d8a157cfe4b';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const res = await respuesta.json();
+        console.log(res);
+      }
+    }
+    consultarAPI();
+  }, [consultar]);
+
   return (
     <div>
       <Header 
@@ -12,7 +35,11 @@ function App() {
         <section className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario 
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">
               2
